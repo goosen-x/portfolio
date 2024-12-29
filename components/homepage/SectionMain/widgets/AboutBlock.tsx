a4c8d2f52f516d9164f111257c63c151ccc1822e
Dmitry Borisenko <brdima92@gmail.com>
2024-12-29 16:51:28 +0300

    fix/ fonts

--- components/homepage/SectionMain/widgets/AboutBlock.tsx
+++ components/homepage/SectionMain/widgets/AboutBlock.tsx
@@ -6,14 +6,14 @@ export const AboutBlock = () => {
 	const t = useTranslations('SectionMain')
 
 	return (
-		<Block className='col-span-12 md:col-span-8 md:row-span-2 text-2xl leading-snug px-2 py-6'>
-			<p className='indent-8 text-pretty text-lg text-accent/60 mr-2'>
+		<Block className='col-span-12 md:col-span-8 md:row-span-2 text-2xl leading-snug px-2 py-6 md:px-6 md:py-6'>
+			<p className='indent-8 text-pretty text-lg text-accent/60 mr-2 md:text-2xl'>
 				{t('markedText')}
 			</p>
-			<p className='text-foreground indent-8 text-pretty text-lg'>
+			<p className='text-foreground indent-8 text-pretty text-lg md:text-2xl'>
 				{t('aboutFirst')}
 			</p>
-			<p className='text-foreground indent-8 text-pretty text-lg'>
+			<p className='text-foreground indent-8 text-pretty text-lg md:text-2xl'>
 				{t('aboutSecond')}
 			</p>
 		</Block>
